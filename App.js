52157872e3e08d62512275803db885509790cb21
hungphu2151 <20110540@student.hcmute.edu.vn>
2024-01-11 15:20:13 +0700

    week1 phu

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
+import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Splash from './components/Splash';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [appIsReady, setAppIsReady] = useState(false);
+
+    useEffect(() => {
+      setTimeout(() => {
+        setAppIsReady(true);
+      }, 10000);
+    }, []);
+  
+  
+    return (
+      <View style={styles.container}>
+        {appIsReady ? <Splash />:<Text>Home 123456</Text>}
+      </View>
+    );
 }
 
 const styles = StyleSheet.create({
